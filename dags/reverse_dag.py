e9370765081f9f58c40f7dad237276a8cb313da4
Alex Volodin <alex-volodin@yandex.ru>
2024-10-02 13:39:46 +0300

    test dag added

---
+++ dags/reverse_dag.py
@@ -0,0 +1,32 @@
+import datetime
+
+from airflow.decorators import dag, task
+from airflow.operators.empty import EmptyOperator
+
+
+@dag(start_date=datetime.datetime(2021, 1, 1), schedule="@once")
+def generate_dag():
+    #EmptyOperator(task_id="task")
+
+    @task
+    def extract():
+        data = {"id":1}
+        print("extract",data)
+        return data
+    
+    @task
+    def transform(data):
+        data2 = {"id":2}
+        print("transform",data,data2)
+        return data2
+    
+    @task
+    def load(data):
+        print("load",data)
+        pass
+    
+    data = extract()
+    data = transform(data)
+    load(data)
+    
+generate_dag()
