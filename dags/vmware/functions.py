351b2723e2ddeb0e0402c4babd2c74e29c7838cc
Alex Volodin <alex-volodin@yandex.ru>
2024-10-08 16:03:24 +0300

    remove try-catch

--- dags/vmware/functions.py
+++ dags/vmware/functions.py
@@ -5,30 +5,25 @@ from pyVmomi import vim, VmomiSupport
 import ssl
 import yaml
 
+
 def vsphere_connect(host, user, pwd):
     print("01", "connecting to", host, user)
     connection_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     connection_context.verify_mode = ssl.CERT_NONE
     print("02", "connection prepared")
-    try:
-        print("03", "establishing connection")
-        connection = SmartConnect(
-            host=host, user=user, pwd=pwd, sslContext=connection_context
-        )
-        print("04", "connection established")
-        content = connection.content
-        print("05", "content length", len(content))
-        return content
-    except Exception as err:
-        print(err)
-    return None
+    print("03", "establishing connection")
+    connection = SmartConnect(
+        host=host, user=user, pwd=pwd, sslContext=connection_context)
+    print("04", "connection established")
+    content = connection.content
+    print("05", "content length", len(content))
+    return content
 
 
 def save(object, path, name):
     filename = "{}/{}.yaml".format(path, name)
     with open(filename, "w", encoding="utf-8") as outfile:
-        yaml.dump(
-            object, outfile, allow_unicode=True, encoding="utf-8", sort_keys=False
-        )
+        yaml.dump(object, outfile, allow_unicode=True,
+                  encoding="utf-8", sort_keys=False)
         print("saved", filename)
     return filename
