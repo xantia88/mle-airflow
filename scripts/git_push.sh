105fd5f2a5e63375a4e971bbc15bb1c4af9be96a
Alex Volodin <alex-volodin@yandex.ru>
2024-10-22 15:28:02 +0300

    fix git push

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-# Настройки
+NOW=$( date '+%F_%H:%M:%S' )
 GIT_URL=
+GIT_PROJECT=
 GIT_USER=
 GIT_TOKEN=
-GIT_PROJECT=
-GIT_USER_EMAIL=""
-GIT_USER_NAME=""
+GIT_USER_EMAIL=''
+GIT_USER_NAME=''
 
-# Вывести информацию о папке, в которой лежат сформированные файлы
-echo "Output dir:"$1
+pwd
 ls -la $1
 
-# Клонировать репозиторий
 rm -r --force --interactive=never $GIT_PROJECT
-echo "git clone https://$GIT_USER:$GIT_TOKEN@$GIT_URL/$GIT_PROJECT.git"
-git clone https://$GIT_USER:$GIT_TOKEN@$GIT_URL/$GIT_PROJECT.git
 
-# Настроить репозиторий
-cd $GIT_PROJECT
-echo "Repo dir:"$( pwd )
+echo "git clone https://$GIT_USER:$GIT_TOKEN@$GIT_URL/$GIT_PROJECT.git"
+git clone https://$GIT_USER:$GIT_TOKEN@$GIT_URL/$GIT_PROJECT.git ./$GIT_PROJECT
+
+cp -v $1/*.yaml ./$GIT_PROJECT
+
+cd ./$GIT_PROJECT
 git config user.email $GIT_USER_EMAIL
 git config user.name $GIT_USER_NAME
-
-# Скопировать файлы
-echo "Copy from:"$1
-cp $1/*.yaml .
 ls -la
-
-# Отправляем файлы в репозиторий на сервер
 git add --all
-git commit -m "airflow commit "$( date '+%F_%H:%M:%S' )
+git commit -m "airflow commit "$NOW
 git push origin
+cd ..
+
+rm -r --force --interactive=never $GIT_PROJECT
